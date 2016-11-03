4e94c981fb1a72c0a5d8ca25705c113f1d65f27a
Lukas Blaho <lblaho@sygic.com>
2016-11-03 15:04:10 +0100

    JsonSerializer try to deserialize custom object based on constructor params

---
+++ src/Serializer/Helper/ParamDeserializationHelper.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace CQRS\Serializer\Helper;
+
+use ReflectionParameter;
+use Zend\Filter\Word\CamelCaseToUnderscore;
+
+final class ParamDeserializationHelper
+{
+    /**
+     * @param array $data
+     * @param ReflectionParameter $parameter
+     * @return mixed
+     */
+    public function deserializeParam(array $data, ReflectionParameter $parameter)
+    {
+        $value = $this->getParamValue($data, $parameter);
+
+        $class = $parameter->getClass();
+        if (!$class || ($value === null && $parameter->allowsNull())) {
+            return $value;
+        }
+
+        return $this->deserializeValue($value, $class->getName());
+    }
+
+    /**
+     * @param array $data
+     * @param ReflectionParameter $parameter
+     * @return mixed
+     */
+    private function getParamValue(array $data, ReflectionParameter $parameter)
+    {
+        $key = $parameter->getName();
+        $value = $this->getValue($data, $key);
+
+        // BC for events serialized with generically named aggregate ID field
+        if ($value === null
+            && $key !== 'aggregateId'
+            && $parameter->getPosition() === 0
+            && !$parameter->allowsNull()
+        ) {
+            $value = $this->getValue($data, 'aggregateId');
+        }
+
+        return $value;
+    }
+
+    /**
+     * @param array $data
+     * @param string $key
+     * @return mixed
+     */
+    private function getValue(array $data, string $key)
+    {
+        return $data[$key] ?? $data[$this->camelCaseToUnderscore($key)] ?? null;
+    }
+
+    private function camelCaseToUnderscore(string $key): string
+    {
+        $filter = new CamelCaseToUnderscore();
+        return strtolower($filter($key));
+    }
+
+    /**
+     * @param mixed $value
+     * @param string $type
+     * @return mixed
+     */
+    private function deserializeValue($value, string $type)
+    {
+        if (method_exists($type, 'jsonDeserialize')) {
+            return $type::jsonDeserialize($value, $this);
+        }
+
+        if (is_string($value)) {
+            if (method_exists($type, 'fromString')) {
+                return $type::fromString($value);
+            }
+        } elseif (is_int($value)) {
+            if (method_exists($type, 'fromInteger')) {
+                return $type::fromInteger($value);
+            }
+
+            if (method_exists($type, 'fromInt')) {
+                return $type::fromInt($value);
+            }
+        } elseif (null === $value && method_exists($type, 'unknown')) {
+            return $type::unknown();
+        }
+
+        return new $type($value);
+    }
+}
