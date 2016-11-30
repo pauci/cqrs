5d097187e70bfe5c6457a4e9b6c5a8d67456c9b1
Pavol Kirschbaum <pkirschbaum@sygic.com>
2016-11-30 12:27:33 +0100

    Change the type of aggregate_id column to binary (to allow to store uuid in binary representation)

--- src/Plugin/Doctrine/EventStore/TableEventStoreSchema.php
+++ src/Plugin/Doctrine/EventStore/TableEventStoreSchema.php
@@ -7,7 +7,9 @@ use Doctrine\DBAL\Schema\Table;
 
 class TableEventStoreSchema
 {
-    /** @var string */
+    /**
+     * @var string
+     */
     private $table;
 
     /**
@@ -30,7 +32,7 @@ class TableEventStoreSchema
         $table->addColumn('event_date', 'datetime', ['notnull' => true]);
         $table->addColumn('event_date_u', 'integer', ['unsigned' => true, 'notnull' => true]);
         $table->addColumn('aggregate_type', 'string', ['length' => 255, 'notnull' => false]);
-        $table->addColumn('aggregate_id', 'string', ['length' => 36, 'notnull' => false]);
+        $table->addColumn('aggregate_id', 'binary', ['length' => 36, 'notnull' => false]);
         $table->addColumn('sequence_number', 'integer', ['unsigned' => true, 'notnull' => false]);
         $table->addColumn('payload_type', 'string', ['length' => 255, 'notnull' => true]);
         $table->addColumn('payload', 'text');
