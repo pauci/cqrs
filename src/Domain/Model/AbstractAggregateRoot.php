0accd2eb6e9b46c6c07257065236eb98baeb6620
Pavol Kirschbaum <pkirschbaum@sygic.com>
2017-11-27 14:46:22 +0100

    Extract AbstractAggregateRoot methods and properties into a trait

--- src/Domain/Model/AbstractAggregateRoot.php
+++ src/Domain/Model/AbstractAggregateRoot.php
@@ -2,10 +2,6 @@
 
 namespace CQRS\Domain\Model;
 
-use CQRS\Domain\Message\DomainEventMessageInterface;
-use CQRS\Domain\Message\Metadata;
-use CQRS\Domain\Payload\AbstractDomainEvent;
-use CQRS\Exception\RuntimeException;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,18 +9,8 @@ use Doctrine\ORM\Mapping as ORM;
  */
 abstract class AbstractAggregateRoot implements AggregateRootInterface, DeletableInterface
 {
+    use AggregateRootTrait;
     use DeletableTrait;
-
-    /**
-     * @var EventContainer
-     */
-    private $eventContainer;
-
-    /**
-     * @ORM\Column(type="integer", options={"unsigned"=true})
-     * @var int
-     */
-    private $lastEventSequenceNumber;
 
     /**
      * @ORM\Version
@@ -34,100 +20,10 @@ abstract class AbstractAggregateRoot implements AggregateRootInterface, Deletabl
     private $version;
 
     /**
-     * @return mixed
-     */
-    abstract public function getId();
-
-    /**
-     * Registers an event to be published when the aggregate is saved, containing the given payload and optional
-     * metadata.
-     *
-     * @param object $payload
-     * @param Metadata|array $metadata
-     * @return DomainEventMessageInterface
-     */
-    protected function registerEvent($payload, $metadata = null)
-    {
-        if ($payload instanceof AbstractDomainEvent && null === $payload->aggregateId) {
-            $payload->setAggregateId($this->getId());
-        }
-
-        return $this->getEventContainer()
-            ->addEvent($payload, $metadata);
-    }
-
-    /**
-     * @param DomainEventMessageInterface $eventMessage
-     * @return DomainEventMessageInterface
-     */
-    protected function registerEventMessage(DomainEventMessageInterface $eventMessage)
-    {
-        return $this->getEventContainer()
-            ->addEventMessage($eventMessage);
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @return DomainEventMessageInterface[]
-     */
-    public function getUncommittedEvents()
-    {
-        if ($this->eventContainer === null) {
-            return [];
-        }
-        return $this->eventContainer->getEvents();
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @return int
-     */
-    public function getUncommittedEventsCount()
-    {
-        return count($this->eventContainer);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function commitEvents()
-    {
-        if ($this->eventContainer !== null) {
-            $this->lastEventSequenceNumber = $this->eventContainer->getLastSequenceNumber();
-            $this->eventContainer->commit();
-        }
-    }
-
-    /**
      * @return int
      */
     public function getVersion()
     {
         return $this->version;
-    }
-
-    /**
-     * @return EventContainer
-     * @throws RuntimeException
-     */
-    private function getEventContainer()
-    {
-        if ($this->eventContainer === null) {
-            $aggregateId = $this->getId();
-            $aggregateType = get_class($this);
-            if ($aggregateId === null) {
-                throw new RuntimeException(sprintf(
-                    'Aggregate ID is unknown in %s. '
-                    . 'Make sure the Aggregate ID is initialized before registering events.',
-                    $aggregateType
-                ));
-            }
-
-            $this->eventContainer = new EventContainer($aggregateType, $aggregateId);
-            $this->eventContainer->initializeSequenceNumber($this->lastEventSequenceNumber);
-        }
-        return $this->eventContainer;
     }
 }
---
+++ src/Domain/Model/AggregateRootTrait.php
@@ -0,0 +1,113 @@
+<?php
+declare(strict_types=1);
+
+namespace CQRS\Domain\Model;
+
+use CQRS\Domain\Message\DomainEventMessageInterface;
+use CQRS\Domain\Message\Metadata;
+use CQRS\Domain\Payload\AbstractDomainEvent;
+use CQRS\Exception\RuntimeException;
+
+trait AggregateRootTrait
+{
+    /**
+     * @var EventContainer
+     */
+    private $eventContainer;
+
+    /**
+     * @ORM\Column(type="integer", options={"unsigned"=true})
+     * @var int
+     */
+    private $lastEventSequenceNumber;
+
+    /**
+     * @return mixed
+     */
+    abstract public function getId();
+
+    /**
+     * Registers an event to be published when the aggregate is saved, containing the given payload and optional
+     * metadata.
+     *
+     * @param object $payload
+     * @param Metadata|array $metadata
+     * @return DomainEventMessageInterface
+     */
+    protected function registerEvent($payload, $metadata = null)
+    {
+        if ($payload instanceof AbstractDomainEvent && null === $payload->aggregateId) {
+            $payload->setAggregateId($this->getId());
+        }
+
+        return $this->getEventContainer()
+            ->addEvent($payload, $metadata);
+    }
+
+    /**
+     * @param DomainEventMessageInterface $eventMessage
+     * @return DomainEventMessageInterface
+     */
+    protected function registerEventMessage(DomainEventMessageInterface $eventMessage)
+    {
+        return $this->getEventContainer()
+            ->addEventMessage($eventMessage);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return DomainEventMessageInterface[]
+     */
+    public function getUncommittedEvents()
+    {
+        if ($this->eventContainer === null) {
+            return [];
+        }
+        return $this->eventContainer->getEvents();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return int
+     */
+    public function getUncommittedEventsCount()
+    {
+        return count($this->eventContainer);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function commitEvents()
+    {
+        if ($this->eventContainer !== null) {
+            $this->lastEventSequenceNumber = $this->eventContainer->getLastSequenceNumber();
+            $this->eventContainer->commit();
+        }
+    }
+
+    /**
+     * @return EventContainer
+     * @throws RuntimeException
+     */
+    private function getEventContainer()
+    {
+        if ($this->eventContainer === null) {
+            $aggregateId = $this->getId();
+            $aggregateType = get_class($this);
+            if ($aggregateId === null) {
+                throw new RuntimeException(sprintf(
+                    'Aggregate ID is unknown in %s. '
+                    . 'Make sure the Aggregate ID is initialized before registering events.',
+                    $aggregateType
+                ));
+            }
+
+            $this->eventContainer = new EventContainer($aggregateType, $aggregateId);
+            $this->eventContainer->initializeSequenceNumber($this->lastEventSequenceNumber);
+        }
+        return $this->eventContainer;
+    }
+}
